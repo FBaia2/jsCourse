196787d712f7e29e7b37e1579d0583c46e80c006
FilipeBaiaBaia <67919144+FilipeBaiaBaia@users.noreply.github.com>
2023-02-01 13:45:48 +0000

    this is another commit

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -48,4 +48,3 @@ console.log(population > avgPopulation)
 
 let description = country + " is in " + continent + " and its " + population + " people speak " + language;
 console.log(description)
-wkodok
