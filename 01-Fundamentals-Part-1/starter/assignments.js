cde579df135f48f1a27359d6a5864c3c58b85eba
FilipeBaiaBaia <67919144+FilipeBaiaBaia@users.noreply.github.com>
2023-02-01 16:42:16 +0000

    17 done did dunzo

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,50 +1,77 @@
 
-// let country = ("Portugal");
-let continent = ("Europe");
-// let population = (10000000);
-// console.log(country + continent + population);
+// // let country = ("Portugal");
+// let continent = ("Europe");
+// // let population = (10000000);
+// // console.log(country + continent + population);
 
-// true
-// let thisThat = true;
-// console.log(typeof thisThat);
-// let thisssThat;
-// console.log(typeof thisssThat)
+// // true
+// // let thisThat = true;
+// // console.log(typeof thisThat);
+// // let thisssThat;
+// // console.log(typeof thisssThat)
 
-// thisThat = "hey";
-// console.log(thisThat)
+// // thisThat = "hey";
+// // console.log(thisThat)
 
-// thisThat ="jejej";
-// console.log(thisThat)
-
-
+// // thisThat ="jejej";
+// // console.log(thisThat)
 
 
-const country = ("Portugal");
-const population = (10000000);
-const language = ("Portuguese");
-const isIsland = true;
-console.log(country);
-console.log(population);
-console.log(language);
-console.log(isIsland);
 
-const futureYear = 2030
-const ageFilipe = futureYear  - 1999;
-const ageErica =  futureYear - 2006;
-const ageKyle = futureYear - 1998;
-console.log(ageFilipe, "hey", ageErica, ageKyle);
 
-console.log(ageFilipe * 2, ageErica / 2, ageKyle * ageErica);
+// const country = ("Portugal");
+// const population = (10000000);
+// const language = ("Portuguese");
+// const isIsland = true;
+// console.log(country);
+// console.log(population);
+// console.log(language);
+// console.log(isIsland);
 
-let halfPopulation = population / 2;
-halfPopulation ++;
-console.log(halfPopulation)
+// const futureYear = 2030
+// const ageFilipe = futureYear  - 1999;
+// const ageErica =  futureYear - 2006;
+// const ageKyle = futureYear - 1998;
+// console.log(ageFilipe, "hey", ageErica, ageKyle);
 
-const finland = 6000000;
-console.log(population > finland)
+// console.log(ageFilipe * 2, ageErica / 2, ageKyle * ageErica);
 
-let avgPopulation = 33000000;
-console.log(population > avgPopulation)
+// let halfPopulation = population / 2;
+// halfPopulation ++;
+// console.log(halfPopulation)
 
-let description = country + " is in " + continent + " and its " + population + " people speak " + language;
-console.log(description)
+// const finland = 6000000;
+// console.log(population > finland)
+
+// let avgPopulation = 33000000;
+// console.log(population > avgPopulation)
+
+// let description = country + " is in " + continent + " and its " + population + " people speak " + language;
+// console.log(description)
+
+
+
+// let description1 = `${country} is in  ${continent} and its ${population} people speak ${language} 
+// }`
+
+// // const markWeight = 78;
+// // const markHeight = 1.69;
+// // // const johnWeight = 92;
+// // // const johnHeight = 1.95;
+
+
+// // // const markBMI =  markWeight / (markHeight ** 2);
+// // // console.log(markBMI)
+// // // const johnBMI =  johnWeight / (johnHeight ** 2);
+// // // console.log(johnBMI)
+
+// // // let markHigherBMI = (markBMI > johnBMI );
+// // // console.log(markHigherBMI)
+// // let firstName = "this";
+// // const jonasNew = `I'm ${firstName} and this is a operation ${markHeight + markWeight}`
+// // console.log(jonasNew)
+
+
+// // console.log("this")
+// // // console.log()
+
