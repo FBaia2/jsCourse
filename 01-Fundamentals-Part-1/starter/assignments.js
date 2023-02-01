fa8477c491a5d4f45be1552f7e8f8edc291b1157
FilipeBaiaBaia <67919144+FilipeBaiaBaia@users.noreply.github.com>
2023-02-01 13:36:30 +0000

    intial commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,50 @@
+
+// let country = ("Portugal");
+let continent = ("Europe");
+// let population = (10000000);
+// console.log(country + continent + population);
+
+// true
+// let thisThat = true;
+// console.log(typeof thisThat);
+// let thisssThat;
+// console.log(typeof thisssThat)
+
+// thisThat = "hey";
+// console.log(thisThat)
+
+// thisThat ="jejej";
+// console.log(thisThat)
+
+
+
+
+const country = ("Portugal");
+const population = (10000000);
+const language = ("Portuguese");
+const isIsland = true;
+console.log(country);
+console.log(population);
+console.log(language);
+console.log(isIsland);
+
+const futureYear = 2030
+const ageFilipe = futureYear  - 1999;
+const ageErica =  futureYear - 2006;
+const ageKyle = futureYear - 1998;
+console.log(ageFilipe, "hey", ageErica, ageKyle);
+
+console.log(ageFilipe * 2, ageErica / 2, ageKyle * ageErica);
+
+let halfPopulation = population / 2;
+halfPopulation ++;
+console.log(halfPopulation)
+
+const finland = 6000000;
+console.log(population > finland)
+
+let avgPopulation = 33000000;
+console.log(population > avgPopulation)
+
+let description = country + " is in " + continent + " and its " + population + " people speak " + language;
+console.log(description)
