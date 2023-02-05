5f17c6f250e75746cb15c7294ae9db8f3807aa6d
FilipeBaiaBaia <67919144+FilipeBaiaBaia@users.noreply.github.com>
2023-02-05 10:21:45 +0000

    034 Complete

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// function logger() {
+//     console.log("My name is jonas")
+// }
+
+// logger();
+// logger();
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(banana, apple) {
+//     const juice = `juice with ${banana} bananas and ${apple} apples`;
+//     return juice;
+// }
+
+
+
+
+
+
+// function describeCountry(country, population, capitalCity) {
+//         const descriptions = `${country} has ${population} people and it's capital city is ${capitalCity}`;
+//         return descriptions;
+// }
+
+
+// const myCountry = describeCountry("Portugal", "6 million", "Lisbon");
+// const theyCountry = describeCountry("sweden", "3 million", "Lisbn");
+// const yourCountry = describeCountry("oslo", "2 million", "isbon");
+
+
+
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// // 
+
+// function calcAge2(birthYear) {
+//     const age2 = 2037 - birthYear;
+//     return age2;
+    
+// }
+
+// const age2= calcAge2(1991)
+// console.log(age1);
+
+// // 
+
+// const calcAge3 = function(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+
+// function percentageOfWorld1(population) {
+//     return  population * 100 / 7900;
+// }
+
+// const portugal = percentageOfWorld1(11);
+
+// console.log(portugal);
+
+// const percentageOfWorld2 = function(population2) {
+//     return  population2 * 100 / 7900;
+// }
+
+// const populus2 = percentageOfWorld2(11);
+// console.log(populus2)
