801e5201f3482f132f6d7b59f13f68eabb353f65
FilipeBaiaBaia <67919144+FilipeBaiaBaia@users.noreply.github.com>
2023-02-06 19:32:49 +0000

    Array lecture completed

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -59,9 +59,7 @@
 // console.log(age3);
 
 
-// function percentageOfWorld1(population) {
-//     return  population * 100 / 7900;
-// }
+
 
 // const portugal = percentageOfWorld1(11);
 
@@ -73,3 +71,112 @@
 
 // const populus2 = percentageOfWorld2(11);
 // console.log(populus2)
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(2000);
+// console.log(age3);
+
+// const percentageOfWorld3 = population3 => population3 * 100 / 7900;
+// const tshhs = percentageOfWorld3(11)
+// console.log(tshhs)
+
+// function percentageOfWorld1(numba) {
+//     return  numba * 100 / 7900;
+// }
+
+// const describePopulation = function(country, population) {
+//     const percent1 = percentageOfWorld1(population);
+//     const final = `${country} has ${population} million people, wich is about ${percent1} % of the world`;
+//     console.log(final)
+// }
+
+
+// describePopulation("Portugal", 11);
+// describePopulation("Spain", 50);
+// describePopulation("USA", 110);
+
+// // 
+
+// function percentageOfWorld1(numba) {
+//     return  numba * 100 / 7900;
+// }
+
+// const describePopulation = function(country, population) {
+//     const percent1 = percentageOfWorld1(population);
+//     const final = `${country} has ${population} million people, wich is about ${percent1} % of the world`;
+//     return final;
+// }
+
+
+// console.log(describePopulation("Portugal", 11));
+// console.log(describePopulation("Spain", 50));
+// console.log(describePopulation("USA", 110));
+
+// const calcAverage = (param1, param2, param3) => {
+//     const avg = (param1 + param2 + param3) / 3
+//     return avg;
+// };
+
+
+// const avgDolphins = calcAverage(44, 23 , 71);
+// const avgKoalas=  calcAverage(85, 54, 41);
+// console.log(avgDolphins, avgKoalas)
+
+
+
+
+// const checkWinner = function(avgDolphins, avgKoalas) {
+//     if (avgDolphins >=  avgKoalas){
+//         console.log(`your gay`);
+//     } else if (avgKoalas > avgDolphins){
+//         console.log(`your mom`)
+        
+//     } else {
+//         return -1;
+
+//     }
+// }
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+
+// const calcAverage = (param1, param2, param3) => {
+//     const avg = (param1 + param2 + param3) / 3
+//     return avg;
+// };
+
+// // Test 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(650, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log('No team wins...');
+//   }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
+
+// const friends = [1,2,3,4,5,6,7,8,9]
+// friends[friends.length -1] = 4
+// console.log(friends)
+// const firstName = "filipe"
+// const filipe = [firstName, "baia-baia", 2023-1999, "teacher", friends]
+// console.log(filipe);
+
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1999, 2000, 2001, 2002, 2003];
+
+
+const ages = [calcAge(years[0]),calcAge(years[0]), calcAge(years[0])];
+console.log(ages)
+
